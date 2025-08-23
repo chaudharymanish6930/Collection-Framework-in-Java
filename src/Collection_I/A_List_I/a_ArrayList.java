7a4090a43f111964edde121ec5bd81c495a1b114
Manish Choudhary <me.developer4u@gmail.com>
2025-08-23 20:41:21 +0530

    Update

---
+++ src/Collection_I/A_List_I/a_ArrayList.java
@@ -0,0 +1,12 @@
+package Collection_I.A_List_I;
+
+import java.util.ArrayList;
+
+public class a_ArrayList {
+    public static void main(String[] args){
+        ArrayList al = new ArrayList();
+        al.add(10);
+        al.addFirst(90);
+        System.out.println(al);
+    }
+}
