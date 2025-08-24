bc898b19a8988da2ce289450c49bbe377b872a80
Manish Choudhary <me.developer4u@gmail.com>
2025-08-24 20:40:03 +0530

    Update

---
+++ src/Collection_Framework/A_List_I/a_ArrayList.java
@@ -0,0 +1,13 @@
+package Collection_Framework.A_List_I;
+
+import java.util.ArrayList;
+
+public class a_ArrayList {
+    public static void main(String[] args){
+        ArrayList<String> al = new ArrayList<String>();
+        al.add("Red");
+        al.addFirst("Yellow");
+        al.add(2,"green");
+        System.out.println(al);
+    }
+}
