dcd0d4ffc85a934d11e3f4926f62548972a307f9
Manish Choudhary <me.developer4u@gmail.com>
2025-08-25 13:05:01 +0530

    Update

---
+++ src/Collection_Framework/B_Cursors_in_Collection/A_Iterator/a_ExampleIterator.java
@@ -0,0 +1,34 @@
+package Collection_Framework.B_Cursors_in_Collection.A_Iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class a_ExampleIterator {
+    public static void main(String[] args) {
+        // Create an ArrayList
+        ArrayList<String> fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+        fruits.add("Orange");
+
+        // Get the iterator
+        Iterator<String> it = fruits.iterator();
+
+        System.out.println("Fruits list using Iterator:");
+
+        // Traverse elements using Iterator
+        while (it.hasNext()) {
+            String fruit = it.next();
+            System.out.println(fruit);
+
+            // Example: remove Mango
+            if (fruit.equals("Mango")) {
+                it.remove();
+            }
+        }
+
+        System.out.print("\nAfter removing Mango: ");
+        System.out.println(fruits);
+    }
+}
---
+++ src/Collection_Framework/B_Cursors_in_Collection/A_Iterator/b_example.java
@@ -0,0 +1,23 @@
+package Collection_Framework.B_Cursors_in_Collection.A_Iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class b_example {
+    public static void main(String[] args){
+        ArrayList nums = new ArrayList();
+
+        nums.add(10);
+        nums.add(20);
+        nums.add(30);
+        nums.add(40);
+        nums.add(50);
+
+        Iterator its = nums.iterator();
+            System.out.print("list using Iterator:");
+            // Traverse elements using Iterator
+            while (its.hasNext()) {
+                System.out.print(its.next()+" ");
+            }
+    }
+}
