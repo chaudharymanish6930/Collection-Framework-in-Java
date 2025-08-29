973e8eb50c7d70c7546b03e4b4e4c928676a72eb
Manish Choudhary <me.developer4u@gmail.com>
2025-08-29 10:18:15 +0530

    Update

---
+++ src/Collection_Framework/E_Queue_Interface/A_Priority_Queue/b_MaxHeapExample.java
@@ -0,0 +1,24 @@
+package Collection_Framework.E_Queue_Interface.A_Priority_Queue;
+import  java.util.*;
+public class b_MaxHeapExample {
+    public static void main(String[] args) {
+        // Min-Heap: smallest element always at the head
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        // Adding elements
+        minHeap.add(10);
+        minHeap.add(5);
+        minHeap.add(20);
+        minHeap.add(15);
+
+        System.out.println("Min-Heap elements: " + minHeap);
+
+        // Access head
+        System.out.println("Peek (smallest): " + minHeap.peek());
+
+        // Removing elements
+        while (!minHeap.isEmpty()) {
+            System.out.println("Removed: " + minHeap.poll());
+        }
+    }
+}
