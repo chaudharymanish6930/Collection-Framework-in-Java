b37597c8aa082a504b85f32461daead30905ca97
Manish Choudhary <me.developer4u@gmail.com>
2026-08-23 23:02:54 +0530

    ArrayList in Array

--- src/Collection_Framework/C_List_Interface/b_ArrayListInArray/a_ArrayListInArray.java
+++ src/Collection_Framework/C_List_Interface/b_ArrayListInArray/a_ArrayListInArray.java
@@ -1,4 +1,15 @@
 package Collection_Framework.C_List_Interface.b_ArrayListInArray;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class a_ArrayListInArray {
+    public static void main(String[] args) {
+        List<int[]> list = new ArrayList<>();
+
+        list.add(new int[]{1,6});
+        list.add(new int[]{8,10});
+        list.add(new int[]{15,18});
+        System.out.println(list);
+    }
 }
